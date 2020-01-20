d0aeac61bf650ae787f8c38a1d38447c32415a4a
ihooni <to@ihooni.com>
2020-01-20 00:24:58 -0500

    chore: set app root path and load env file when application start

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"path"
 
+	"github.com/spf13/viper"
+)
+
+func main() {
+	currDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// set app root path as config
+	viper.Set("AppRoot", path.Join(currDir, "../../"))
+
+	// load env file
+	viper.SetConfigFile(path.Join(viper.GetString("AppRoot"), "./env.yml"))
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading env file, %s", err)
+	}
 }
