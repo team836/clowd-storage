a26421171b1ed7ca4201f777b959f174d6e06ede
ihooni <to@ihooni.com>
2020-01-22 16:50:22 -0500

    feat: build main router and use basic middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,9 +4,10 @@ import (
 	"os"
 	"path"
 
-	"github.com/team836/clowd-storage/pkg/logger"
-
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
+	"github.com/team836/clowd-storage/pkg/logger"
 )
 
 func main() {
@@ -23,4 +24,41 @@ func main() {
 	if err := viper.ReadInConfig(); err != nil {
 		logger.Console().Fatalf("Error reading env file, %s", err)
 	}
+
+	router := buildRouter()
+}
+
+func buildRouter() *echo.Echo {
+	// open the access log file
+	accessLogFile, err := os.OpenFile(
+		path.Join(viper.GetString("AppRoot"), "/logs/access.log"),
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
+		0666,
+	)
+
+	// if cannot open the file, using default stdout
+	if err != nil {
+		logger.Console().Warn("Failed to logging to file(`access.log`), using default stdout")
+		accessLogFile = os.Stdout
+	}
+
+	// basic middleware list which is applied to main router
+	basicMiddleware := []echo.MiddlewareFunc{
+		middleware.BodyLimit("200MB"),
+		middleware.CORS(),
+		middleware.Gzip(),
+		middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: `{"time":"${time_rfc3339_nano}","remote_ip":"${remote_ip}","host":"${host}",` +
+				`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}",` +
+				`"latency":${latency},"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
+			Output: accessLogFile,
+		}),
+		middleware.Recover(),
+		middleware.Secure(),
+	}
+
+	mainRouter := echo.New()
+	mainRouter.Use(basicMiddleware...)
+
+	return mainRouter
 }
