80f81cfed7266ac7b6eaf0c2c8914b13b47c25f0
ihooni <to@ihooni.com>
2020-01-23 23:10:20 -0500

    refactor: create provider package and migrate `buildRouter` function to provider package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,10 +8,8 @@ import (
 	"path"
 	"time"
 
-	"github.com/team836/clowd-storage/internal/api"
+	"github.com/team836/clowd-storage/internal/provider"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
 	"github.com/team836/clowd-storage/pkg/logger"
 )
@@ -32,7 +30,7 @@ func main() {
 	}
 
 	// build all of the router
-	router := buildRouter()
+	router := provider.RouteService()
 
 	// start server
 	go func() {
@@ -58,42 +56,4 @@ func main() {
 	if err := router.Shutdown(ctx); err != nil {
 		logger.File().Error(err)
 	}
-}
-
-func buildRouter() *echo.Echo {
-	// open the access log file
-	accessLogFile, err := os.OpenFile(
-		path.Join(viper.GetString("AppRoot"), "/logs/access.log"),
-		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
-		0666,
-	)
-
-	// if cannot open the file, using default stdout
-	if err != nil {
-		logger.Console().Warn("Failed to logging to file(`access.log`), using default stdout")
-		accessLogFile = os.Stdout
-	}
-
-	// basic middleware list
-	basicMiddleware := []echo.MiddlewareFunc{
-		middleware.BodyLimit("200MB"),
-		middleware.CORS(),
-		middleware.Gzip(),
-		middleware.LoggerWithConfig(middleware.LoggerConfig{
-			Format: `{"time":"${time_rfc3339_nano}","remote_ip":"${remote_ip}","host":"${host}",` +
-				`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}",` +
-				`"latency":${latency},"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
-			Output: accessLogFile,
-		}),
-		middleware.Recover(),
-		middleware.Secure(),
-	}
-
-	// register middleware & handlers
-	mainRouter := echo.New()
-	mainRouter.Use(basicMiddleware...)
-	v1Group := mainRouter.Group("/v1")
-	api.RegisterHandlers(v1Group)
-
-	return mainRouter
 }
