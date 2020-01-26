80264fe1b0bb4f7a62ee0bf5f29d6024bb78ca14
ihooni <to@ihooni.com>
2020-01-25 23:37:57 -0500

    test: websocket test

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,6 +12,35 @@ func RegisterHandlers(group *echo.Group) {
 	group.GET("/test", test)
 }
 
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
 func test(ctx echo.Context) error {
-	return ctx.String(http.StatusOK, "Hello clowd")
+	ws, err := upgrader.Upgrade(ctx.Response(), ctx.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		if err != nil {
+			ctx.Logger().Error(err)
+		}
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			ctx.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+	}
 }
