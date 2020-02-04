b025870841b520a9638ab811698294a92f3d23c0
ihooni <to@ihooni.com>
2020-02-04 15:55:49 -0500

    chore: apply node handlers group

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,46 +1,11 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
+	"github.com/team836/clowd-storage/internal/api/node"
 )
 
 func RegisterHandlers(group *echo.Group) {
-	group.GET("/test", test)
-}
-
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-)
-
-func test(ctx echo.Context) error {
-	ws, err := upgrader.Upgrade(ctx.Response(), ctx.Request(), nil)
-	if err != nil {
-		return err
-	}
-	defer ws.Close()
-
-	for {
-		// Write
-		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-		if err != nil {
-			ctx.Logger().Error(err)
-		}
-
-		// Read
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			ctx.Logger().Error(err)
-		}
-		fmt.Printf("%s\n", msg)
-	}
+	nodeGroup := group.Group("/node")
+	node.RegisterHandlers(nodeGroup)
 }
