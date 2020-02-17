b89a35727ec32e0c9594a5907475a6d139de1e23
ihooni <to@ihooni.com>
2020-02-17 11:11:50 -0500

    feat: add file upload scheduling method

--- internal/api/client/upload-queue.go
+++ internal/api/client/upload-queue.go
@@ -1,6 +1,11 @@
 package client
 
-import "sort"
+import (
+	"container/ring"
+	"sort"
+
+	"github.com/team836/clowd-storage/internal/api/node"
+)
 
 type EncFile struct {
 	fileID string
@@ -30,4 +35,40 @@ func (uq *UploadQueue) sort() {
 	sort.Slice(uq.files, func(i, j int) bool {
 		return len(uq.files[i].data[0]) > len(uq.files[j].data[0])
 	})
+}
+
+/**
+Assign every data shards for saving to the clowders.
+And update metadata and predict clowders' status by scheduling results.
+
+This function change clowders' status. So you SHOULD use this function with
+the `ClowdersStatusLock` which is mutex for all clowders' status.
+*/
+func (uq *UploadQueue) schedule(clowders *ring.Ring) map[*node.Clowder][]*node.FileOnNode {
+	// sort the files to upload before scheduling
+	uq.sort()
+
+	quotas := make(map[*node.Clowder][]*node.FileOnNode)
+	// for every files to save
+	for _, file := range uq.files {
+		// for every shards
+		for _, shard := range file.data {
+			currClowder := clowders.Value.(*node.Clowder)
+
+			// assignment shard to this clowder
+			quotas[currClowder] = append(
+				quotas[currClowder],
+				node.NewFileOnNode("0", shard), // TODO: set valid file name
+			)
+
+			// TODO: save metadata to the database using goroutine
+
+			// clowder status prediction
+			currClowder.Status.Capacity -= uint64(len(shard))
+
+			clowders.Next()
+		}
+	}
+
+	return quotas
 }
