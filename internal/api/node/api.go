2421ecc5e6659df6388650b0584101ecec098b93
ihooni <to@ihooni.com>
2020-02-21 17:24:56 -0500

    fix: duplicate connection

--- internal/api/node/api.go
+++ internal/api/node/api.go
@@ -3,12 +3,10 @@ package node
 import (
 	"net/http"
 
-	"github.com/team836/clowd-storage/internal/model"
-
-	"github.com/team836/clowd-storage/pkg/logger"
-
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
+	"github.com/team836/clowd-storage/internal/model"
+	"github.com/team836/clowd-storage/pkg/logger"
 )
 
 var (
@@ -31,6 +29,21 @@ func RegisterHandlers(group *echo.Group) {
 Handle the websocket requests from the clowder.
 */
 func openWebsocket(ctx echo.Context) error {
+	clowderModel := ctx.Get("clowder").(*model.Clowder) // get current clowder model
+
+	machineID := ctx.QueryParam("mid") // get clowder machine id
+	if machineID == "" {
+		return ctx.String(http.StatusBadRequest, "Cannot find the machine id at the query parameters")
+	}
+
+	// find duplicate clowder connection and unregistered old one
+	for clowder := range Pool().clowders {
+		if clowder.machineID == machineID {
+			Pool().unregister <- clowder
+			break
+		}
+	}
+
 	// upgrade to websocket protocol
 	conn, err := upgrader.Upgrade(ctx.Response(), ctx.Request(), nil)
 	if err != nil {
@@ -39,7 +52,7 @@ func openWebsocket(ctx echo.Context) error {
 	}
 
 	// create new clowder
-	clowder := NewClowder(conn, ctx.Get("clowder").(*model.Clowder))
+	clowder := NewClowder(machineID, conn, clowderModel)
 
 	go clowder.run()           // run the websocket operations
 	Pool().register <- clowder // register this clowder to pool
