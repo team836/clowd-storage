a3dea78fc96dc97ffa63a246c280243a4ada903b
ihooni <to@ihooni.com>
2020-02-21 23:12:03 -0500

    chore: add middleware for node api group

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,12 +3,13 @@ package api
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/team836/clowd-storage/internal/api/client"
+	"github.com/team836/clowd-storage/internal/api/middleware"
 	"github.com/team836/clowd-storage/internal/api/middleware/auth"
 	"github.com/team836/clowd-storage/internal/api/node"
 )
 
 func RegisterHandlers(group *echo.Group) {
-	nodeGroup := group.Group("/node", auth.AuthenticateClowder)
+	nodeGroup := group.Group("/node", auth.AuthenticateClowder, middleware.PrepareModel)
 	node.RegisterHandlers(nodeGroup)
 
 	clientGroup := group.Group("/client", auth.AuthenticateClowdee)
---
+++ internal/api/middleware/node.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/team836/clowd-storage/internal/model"
+	"github.com/team836/clowd-storage/pkg/database"
+	"github.com/team836/clowd-storage/pkg/logger"
+)
+
+/**
+Middelware for node api group.
+*/
+
+/**
+Middleware for preparing the node model.
+*/
+func PrepareModel(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		clowderModel := ctx.Get("clowder").(*model.Clowder) // get current clowder model
+		machineID := ctx.QueryParam("mid")                  // get clowder machine id
+		if machineID == "" {
+			return ctx.String(http.StatusBadRequest, "Cannot find the machine id at the query parameters")
+		}
+
+		// create node record if not exists
+		node := &model.Node{}
+		sqlResult := database.Conn().
+			Where(&model.Node{MachineID: machineID}).
+			Attrs(&model.Node{ClowderGoogleID: clowderModel.GoogleID}).
+			FirstOrCreate(node)
+
+		if sqlResult.Error != nil {
+			logger.File().Errorf("Error preparing the node model, %s", sqlResult.Error.Error())
+			return ctx.NoContent(http.StatusInternalServerError)
+		}
+
+		ctx.Set("node", node)
+
+		return next(ctx)
+	}
+}
