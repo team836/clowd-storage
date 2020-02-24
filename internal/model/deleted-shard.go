1fc66fdb8947daed41aabf027024b0d408c82d12
ihooni <to@ihooni.com>
2020-02-24 15:04:17 -0500

    feat: new database model and schema for deleted shard

---
+++ internal/model/deleted-shard.go
@@ -0,0 +1,16 @@
+package model
+
+import "github.com/team836/clowd-storage/pkg/database"
+
+type DeletedShard struct {
+	// column fields
+	Name      string `gorm:"type:varchar(255);primary_key"`
+	MachineID string `gorm:"type:varchar(255);not null"`
+}
+
+func MigrateDeletedShard() {
+	database.Conn().Set("gorm:table_options", "CHARSET=utf8mb4").
+		AutoMigrate(&DeletedShard{}).
+		Model(&DeletedShard{}).
+		AddForeignKey("machine_id", "nodes(machine_id)", "CASCADE", "CASCADE")
+}
